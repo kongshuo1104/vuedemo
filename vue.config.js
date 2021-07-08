cb0a04e6c4d0bc1ebbe930f3d4cb4075e7ed8310
kongshuo <kongshuo@example.com>
2021-07-09 00:12:18 +0800

    登录功能

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//     devServer: {
+//         /* 解决跨域*/
+//         proxy: {
+//             '/api': {
+//                 target: 'https://www.liulongbin.top:8888/api/private/v1/',
+//                 changeOrigin: true,
+//                 secure: true, //是否使用https
+//             }
+//         }
+//     }
+// }
